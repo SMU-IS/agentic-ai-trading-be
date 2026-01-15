d43f4082dca29ec4d66192b0e735272ad76daaa3
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-01-15 16:57:48 +0800

    fix: broken path
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- news-analysis/app/core/config.py
+++ news-analysis/app/core/config.py
@@ -1,8 +1,9 @@
 import os
 
-from app.core.constant import StorageProviders
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from core.constant import StorageProviders
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ENV_PATH = os.path.join(BASE_DIR, ".env")
