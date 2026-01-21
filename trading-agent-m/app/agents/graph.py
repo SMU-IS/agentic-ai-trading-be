f95de4e94a79bab578b9a375709328748acaa13f
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-01-21 10:45:37 +0800

    fix: code clean up
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- trading-agent-m/app/agents/graph.py
+++ trading-agent-m/app/agents/graph.py
@@ -1,12 +1,13 @@
+from app.core.config import env_config
 from app.services.trading_workflow import TradingWorkflow
-from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
-# from app.core.broker import BrokerClient # Assuming you have a broker wrapper
-
-# 1. Initialize your Clients via config/env
-llm = ChatOpenAI(model="gpt-4o", temperature=0)
+# 1. Initialize clients via config/env
+llm = ChatOllama(
+    model=env_config.large_language_model,
+    temperature=env_config.ollama_temperature,
+    base_url=env_config.ollama_base_url,
+)
 broker = None
-
-
 trading_agent = TradingWorkflow(llm_client=llm, broker_client=broker)
 app_workflow = trading_agent
