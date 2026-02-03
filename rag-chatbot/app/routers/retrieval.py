19bcf0008c4bc16e7e9534dd3fd48adb56c32655
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-02-03 19:49:13 +0800

    feat: update to rag chatbot api
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- rag-chatbot/app/routers/retrieval.py
+++ rag-chatbot/app/routers/retrieval.py
@@ -15,21 +15,23 @@ router = APIRouter(tags=["RAG Chatbot"])
 def get_retrieval_service():
     strategy = get_strategy(env_config.llm_provider)
     llm_model = strategy.create_model()
-
     retrieval_service = RetrievalService(llm_model)
 
     return retrieval_service
 
 
-@router.post(APIPath.CHAT)
+# general query
+
+
+# query + order_id
+@router.post(APIPath.ORDER)
 async def chat_stream(
     request: ChatRequest,
     retrieval_service: RetrievalService = Depends(get_retrieval_service),
 ):
     return StreamingResponse(
-        retrieval_service.get_answer_stream(
-            query=request.message,
-            tickers=request.tickers,
+        retrieval_service.fetch_order_details_augment_response(
+            query=request.query, order_id=request.order_id
         ),
         media_type="text/event-stream",
     )
