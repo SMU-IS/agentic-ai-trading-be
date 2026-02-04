342322890f30d9fad96f53086eea688968e34c32
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-02-04 09:40:43 +0800

    chore: refine llm tool doc string
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- rag-chatbot/app/services/tools/__init__.py
+++ rag-chatbot/app/services/tools/__init__.py
@@ -1,6 +1,6 @@
 from .database_tools import (
-    get_agent_m_transactions,
     get_general_news_context_and_result,
+    get_trade_history_details,
 )
 
-RAG_BOT_TOOLS = [get_agent_m_transactions, get_general_news_context_and_result]
+RAG_BOT_TOOLS = [get_trade_history_details, get_general_news_context_and_result]
