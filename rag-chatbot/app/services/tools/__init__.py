95eaa6d8e95c2f1666f6994d972ccae55bea0bfc
Joshua <cxang.2022@scis.smu.edu.sg>
2026-02-04 09:18:31 +0800

    chore: update prompt (#99)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- rag-chatbot/app/services/tools/__init__.py
+++ rag-chatbot/app/services/tools/__init__.py
@@ -1,3 +1,6 @@
-from .database_tools import get_agent_m_transactions
+from .database_tools import (
+    get_agent_m_transactions,
+    get_general_news_context_and_result,
+)
 
-RAG_BOT_TOOLS = [get_agent_m_transactions]
+RAG_BOT_TOOLS = [get_agent_m_transactions, get_general_news_context_and_result]
