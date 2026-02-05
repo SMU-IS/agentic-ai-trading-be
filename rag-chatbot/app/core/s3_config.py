d5d91beb271d0ae63a94bb6150881f8129c1e8e9
Joshua <cxang.2022@scis.smu.edu.sg>
2026-02-05 11:14:26 +0800

    feat: use s3 for prompt template (#108)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

---
+++ rag-chatbot/app/core/s3_config.py
@@ -0,0 +1,21 @@
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+
+from app.core.config import env_config
+
+
+class S3ConfigService:
+    def __init__(self):
+        self.s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=env_config.aws_access_key_id,
+            aws_secret_access_key=env_config.aws_secret_access_key,
+        )
+
+    def get_file_content(self, bucket: str, key: str) -> str:
+        try:
+            response = self.s3_client.get_object(Bucket=bucket, Key=key)
+            return response["Body"].read().decode("utf-8")
+        except (BotoCoreError, ClientError) as e:
+            print(f"S3 Retrieval Error: {e}")
+            raise
