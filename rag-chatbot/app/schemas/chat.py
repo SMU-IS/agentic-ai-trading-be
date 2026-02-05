6fae6e7808a47a724f197fa1b2baf96a1543d497
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-02-05 10:07:46 +0800

    fix: update prompt and llm logic
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- rag-chatbot/app/schemas/chat.py
+++ rag-chatbot/app/schemas/chat.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import BaseModel
 from pydantic.v1.fields import Field
 
@@ -5,3 +7,27 @@ from pydantic.v1.fields import Field
 class ChatRequest(BaseModel):
     query: str = Field(..., description="User query to the LLM")
     order_id: str | None = None
+
+
+class GeneralNews(BaseModel):
+    query: str = Field(
+        ...,
+        description="The specific topic, question, or search string to look for in the news. e.g 'What is the latest news on Apple?'",
+    )
+    tickers: List[str] = Field(
+        description="A list of stock tickers (e.g. ['AAPL', 'TSLA']) to get news for"
+    )
+
+
+class TradeHistory(BaseModel):
+    query: str = Field(
+        ...,
+        description=(
+            "A detailed summary of the position to be analysed, including the "
+            "ticker/symbol, current price, shares, average entry price, and "
+            "Profit/Loss (P/L) data provided by the user."
+        ),
+    )
+    order_id: str = Field(
+        description="The order ID to get the previously executed trade history"
+    )
