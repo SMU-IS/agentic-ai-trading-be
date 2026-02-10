e928872b2e7873a48bfa3367faf952264bb4f5ab
ziyouuu <ziyou.foo.2022@scis.smu.edu.sg>
2026-02-10 15:50:38 +0800

    service setup & notification stream setup

--- notification-alert/app/core/config.py
+++ notification-alert/app/core/config.py
@@ -5,7 +5,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ENV_PATH = os.path.join(BASE_DIR, ".env")
 
-
 class EnvConfig(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=ENV_PATH,
@@ -13,17 +12,11 @@ class EnvConfig(BaseSettings):
         extra="ignore",
     )
 
-    # Redis
     redis_host: str = Field(..., validation_alias="REDIS_HOST")
     redis_port: int = Field(..., validation_alias="REDIS_PORT")
-    redis_stream_name: str = Field(..., validation_alias="REDIS_STREAM")
+    redis_notification_stream: str = Field(..., validation_alias="REDIS_NOTIFICATION_STREAM")
+    redis_sentiment_stream: str = Field(..., validation_alias="REDIS_SENTIMENT_STREAM")
     redis_password: str = Field(..., validate_alias="REDIS_PASSWORD")
-
-    # Reddit API
-    reddit_client_id: str = Field(..., validation_alias="REDDIT_CLIENT_ID")
-    reddit_client_secret: str = Field(..., validation_alias="REDDIT_CLIENT_SECRET")
-    reddit_user_agent: str = Field(..., validation_alias="REDDIT_USER_AGENT")
-
 
 env_config = EnvConfig()
 config = env_config 
---
+++ notification-alert/app/main.py
@@ -0,0 +1,16 @@
+import asyncio
+from fastapi import FastAPI
+from app.workers.sentiment_to_notification import SentimentBridge
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def start_consumers():
+    """
+    Start both Redis consumers when FastAPI starts.
+    """
+    # Sentiment -> Notification bridge
+    sentiment_bridge = SentimentBridge()
+    asyncio.create_task(sentiment_bridge.async_start())
+
+
