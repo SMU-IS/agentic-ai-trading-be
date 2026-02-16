8be4d83bdf4213561b2b7b461671eeb39992f443
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-02-17 06:59:56 +0800

    fix: refactor qdrant retrieval into separate module
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- news-analysis/app/services/orchestration.py
+++ news-analysis/app/services/orchestration.py
@@ -177,7 +177,6 @@ async def run_pipeline():
     )
 
     vectorisation_service = VectorisationService()
-    await vectorisation_service.setup_indexing()
 
     # # Clear streams for testing
     # preprocessing_stream.clear_stream()
