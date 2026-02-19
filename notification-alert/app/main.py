56221601cddfcb78994677cfee761e94d4cadce0
ziyouuu <ziyou.foo.2022@scis.smu.edu.sg>
2026-02-19 12:12:02 +0800

    news_aggregator_stream

--- notification-alert/app/main.py
+++ notification-alert/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.workers.stream_consumer import StreamConsumer
 from app.workers.sentiment_to_notification import SentimentBridge
+from app.workers.sentiment_to_aggregator import SentimentAggregator
 from app.services.notification_service import router as notification_router
 
 
@@ -12,9 +13,11 @@ app.include_router(notification_router)
 
 @app.on_event("startup")
 async def start_consumers():
-    """
-    Start both Redis consumers when FastAPI starts.
-    """
+    
+    # Sentiment -> Aggregator 
+    sentiment_bridge = SentimentAggregator()
+    asyncio.create_task(sentiment_bridge.async_start())
+
     # Sentiment -> Notification bridge
     sentiment_bridge = SentimentBridge()
     asyncio.create_task(sentiment_bridge.async_start())
