9bdb66ed7be3e8a386e7db9d600fb04bbc84d791
Joshua <cxang.2022@scis.smu.edu.sg>
2026-02-21 20:51:02 +0800

    feat(infra): initial infrastructure as code for agentic trading backend (#205)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "services" {
+  for_each             = toset(var.services)
+  name                 = each.value
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,7 @@
+variable "services" {
+  type = list(string)
+}
+
+variable "environment" {
+  type = string
+}
