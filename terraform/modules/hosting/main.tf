489ef2f0942c6b2e1dbe13ebadcd67058eeaf6fc
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-04-08 06:55:54 +0800

    chore: replace acm cert
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- terraform/modules/hosting/main.tf
+++ terraform/modules/hosting/main.tf
@@ -15,11 +15,42 @@ data "aws_route53_zone" "selected" {
 }
 
 #  ACM Certificate
-data "aws_acm_certificate" "issued" {
-  domain      = "api.agentic-m.com"
-  statuses    = ["ISSUED"]
-  most_recent = true
-  provider    = aws.us_east_1
+resource "aws_acm_certificate" "api_cert" {
+  domain_name               = "api.agentic-m.com"
+  subject_alternative_names = []
+  validation_method         = "DNS"
+  provider                  = aws.us_east_1
+
+  tags = {
+    Environment = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.selected.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.api_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  provider                = aws.us_east_1
 }
 
 data "aws_cloudfront_cache_policy" "caching_disabled" {
@@ -127,7 +158,7 @@ resource "aws_cloudfront_distribution" "kong_api" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = data.aws_acm_certificate.issued.arn
+    acm_certificate_arn      = aws_acm_certificate_validation.cert.certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
