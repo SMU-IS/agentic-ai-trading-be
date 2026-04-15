849079e8fcd11bd98dc09892fbdf30a802f68c18
joshydavid <cxang.2022@scis.smu.edu.sg>
2026-04-15 21:40:03 +0800

    infra: upgrade cpu [skip ci]
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- terraform/main.tf
+++ terraform/main.tf
@@ -354,13 +354,13 @@ resource "kubectl_manifest" "karpenter_node_pool" {
             { key = "karpenter.sh/capacity-type", operator = "In", values = ["spot", "on-demand"] },
             { key = "kubernetes.io/arch", operator = "In", values = ["arm64"] },
             { key = "karpenter.k8s.aws/instance-family", operator = "In", values = ["t4g", "c7g", "m7g", "r7g"] },
-            { key = "karpenter.k8s.aws/instance-size", operator = "In", values = ["medium", "large", "xlarge"] }
+            { key = "karpenter.k8s.aws/instance-size", operator = "In", values = ["large", "xlarge", "2xlarge"] }
           ]
         }
       }
       limits = {
-        cpu    = 40
-        memory = "80Gi"
+        cpu    = 80
+        memory = "160Gi"
       }
       disruption = {
         consolidationPolicy = "WhenEmptyOrUnderutilized"
